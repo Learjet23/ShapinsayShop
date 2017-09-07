096c0878ad14b077e84d333b9f3155905d86a475
scott.learjet@gmail.com <Kirsty_23>
2017-09-07 21:44:36 +0100

    Initial commit

---
+++ views/main/index.php
@@ -0,0 +1,16 @@
+<div class="jumbotron jumbo-image jumbo-header" style="background-image: url('https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/18121659_1712404845499680_4115695723148268273_o.jpg?oh=2b4ab3a75d30269e06bfff82727888ca&oe=5A5B425E');">
+    <div class="container">
+        <h1>Thomas Sinclair & Balfour Bakery</h1>
+        <p>Independent General Store & Post Office on the Island of Shapinsay in Orkney.</p>
+    </div>
+</div>
+<div class="container">
+    <div class="col-lg-7 col-md-5 col-sm-4">
+        <ul>
+            <li>We are situated in Balfour Village just a short walk from the Ferry.</li>
+            <li>Outside seating - A great place to view the common seals which live here most of the year.</li>
+            <li>For yachtsmen please note you can take your dinghy almost to our door if the tide is right
+                so you can pick up your stores easily. Orders advised for fresh products to assure availability.</li>
+        </ul>
+    </div>
+</div>
